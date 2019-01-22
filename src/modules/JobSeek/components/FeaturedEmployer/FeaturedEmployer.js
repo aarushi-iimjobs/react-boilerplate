761253eec1627bd207ba6abb2bdc20852151a241
Aarushi <aarushi@iimjobs.com>
2019-01-22 16:35:52 +0530

    Fixed postion of featured employer component, scroll on overflow

--- src/modules/JobSeek/components/FeaturedEmployer/FeaturedEmployer.js
+++ src/modules/JobSeek/components/FeaturedEmployer/FeaturedEmployer.js
@@ -15,24 +15,26 @@ class FeaturedEmployer extends Component {
     return (
       <div>
         <div>
-          <div className="clearfix"></div>
-          <h4 className="txt15 strong pt0">Featured Employers</h4>
-          <GreyDivider />
-          <div className="clearfix"></div>
-          {map(featuredEmployers, employer => (
-            <div className="pull-left mb10 col-md-6 pd0">
-              <span className="pull-left" rel="tooltip" title="" data-original-title={employer.name}>
-                <a rel="nofollow" target="_blank" title={employer.name}>
-                  <img src={employer.url} width="16" height="16" />
-                </a>
-              </span>
-              <span className="pull-left txt13 wd34 ml10 ellip" alt={employer.name} rel="tooltip" title="" data-original-title={employer.name}>
-                <a rel="nofollow" title={employer.name} className="title-employer">
-                  {employer.name}
-                </a>
-              </span>
-            </div>
-          ))}
+          <div className="overflow">
+            <div className="clearfix"></div>
+            <h4 className="txt15 strong pt0">Featured Employers</h4>
+            <GreyDivider />
+            <div className="clearfix"></div>
+            {map(featuredEmployers, employer => (
+              <div className="pull-left mb10 col-md-6 pd0">
+                <span className="pull-left" rel="tooltip" title="" data-original-title={employer.name}>
+                  <a rel="nofollow" target="_blank" title={employer.name}>
+                    <img src={employer.url} width="16" height="16" />
+                  </a>
+                </span>
+                <span className="pull-left txt13 wd34 ml10 ellip" alt={employer.name} rel="tooltip" title="" data-original-title={employer.name}>
+                  <a rel="nofollow" title={employer.name} className="title-employer">
+                    {employer.name}
+                  </a>
+                </span>
+              </div>
+            ))}
+          </div>
           <div className="clearfix"></div>
           <GreyDivider />
           <DiversityEmployer />
