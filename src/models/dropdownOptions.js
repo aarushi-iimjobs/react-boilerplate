71e6423ff90af3fb703c3c43cd68000baa6ac8d5
Aarushi <aarushi@iimjobs.com>
2019-01-28 13:21:49 +0530

    Add more models to use static data

---
+++ src/models/dropdownOptions.js
@@ -0,0 +1,26 @@
+export const postingOptions = [
+  { id: 1, name: '< 3 Days' },
+  { id: 2, name: 'Last Week' },
+  { id: 3, name: 'Two Week' },
+  { id: 4, name: 'Last Month' },
+]
+
+export const experienceOptions = [
+  { id: 0, name: 'Any Exp. Level' },
+  { id: 1, name: '1 - 3 years' },
+  { id: 2, name: '4 - 6 years' },
+  { id: 4, name: '7 - 10 years' },
+  { id: 3, name: '10 - 15 years' },
+  { id: 5, name: '15+ years' },
+]
+
+export const locationOptions = [
+  { id: 'location_87', value: 87, name: 'Metros' },
+  { id: 'location_88', value: 88, name: 'Anywhere in India' },
+  { id: 'location_89', value: 89, name: 'Overseas/International' },
+]
+
+export const relevanceOptions = [
+  { id: 1, name: 'By Date' },
+  { id: 2, name: 'By Relevance' }
+]
---
+++ src/models/trendingCourses.js
@@ -0,0 +1,20 @@
+export const trendingCourses = [
+  {
+    id: 1,
+    name: 'IIFT - Executive Development Program in Strategies for International Business',
+    href: 'https://www.iimjobs.com/course/iift-executive-development-program-in-strategies-for-international-business-1373.html?reforg=search&amp;refinv=search',
+    url: 'https://edgar.iimjobs.com/media/courselogo/2018/10/25/2018-10-25-15-59-51-47815-59697.jpg'
+  }, 
+  {
+    id: 2,
+    name: 'PG Certificate Programme in Business Management From MICA',
+    href: 'https://www.iimjobs.com/course/pg-certificate-programme-in-business-management-from-mica-0-30-yrs-1193.html?reforg=search&amp;refinv=search',
+    url: 'https://edgar.iimjobs.com/media/courselogo/2017/09/29/2017-09-29-13-01-54-10203-59697.jpg'
+  },
+  {
+    id: 3,
+    name: 'XLRI | Executive Development Program in Strategic Management',
+    href: 'https://www.iimjobs.com/course/xlri-executive-development-program-in-strategic-management-1345.html?reforg=search&amp;refinv=search',
+    url: 'https://edgar.iimjobs.com/media/courselogo/2018/08/21/2018-08-21-14-25-07-73579-59697.jpg'
+  }
+]
